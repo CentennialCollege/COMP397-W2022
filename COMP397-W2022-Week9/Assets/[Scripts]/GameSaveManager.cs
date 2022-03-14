933addfc1e583a2923749a2cd132efa24134b462
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-14 18:00:26 -0400

    Modified Save / Load System to use BinaryFormatter

--- COMP397-W2022-Week9/Assets/[Scripts]/GameSaveManager.cs
+++ COMP397-W2022-Week9/Assets/[Scripts]/GameSaveManager.cs
@@ -1,5 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameSaveManager : MonoBehaviour
@@ -22,21 +27,28 @@ public class GameSaveManager : MonoBehaviour
 
     void SaveGame()
     {
-        string playerPosition = JsonUtility.ToJson(player.position);
-        string playerRotation = JsonUtility.ToJson(player.rotation.eulerAngles);
-        PlayerPrefs.SetString("PlayerPosition", playerPosition);
-        PlayerPrefs.SetString("PlayerRotation", playerRotation);
-        PlayerPrefs.Save();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/MySaveData.dat");
+        PlayerData data = new PlayerData();
+        data.position = new[] { player.position.x, player.position.y, player.position.z};
+        data.rotation = new[] { player.rotation.eulerAngles.x, player.rotation.eulerAngles.y, player.rotation.eulerAngles.z };
+        bf.Serialize(file, data);
+        file.Close();
         Debug.Log("Game data saved!");
+        Debug.Log(JsonUtility.ToJson(data));
     }
 
     void LoadGame()
     {
-        if (PlayerPrefs.HasKey("PlayerPosition"))
+        if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
         {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/MySaveData.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
             player.gameObject.GetComponent<CharacterController>().enabled = false;
-            player.position = JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("PlayerPosition"));
-            player.rotation = Quaternion.Euler(JsonUtility.FromJson<Vector3>(PlayerPrefs.GetString("PlayerRotation")));                    
+            player.position = new Vector3(data.position[0], data.position[1], data.position[2]);
+            player.rotation = Quaternion.Euler(data.rotation[0], data.rotation[1], data.rotation[2]);
             player.gameObject.GetComponent<CharacterController>().enabled = true;
             Debug.Log("Game data loaded!");
         }
@@ -48,8 +60,15 @@ public class GameSaveManager : MonoBehaviour
 
     void ResetData()
     {
-        PlayerPrefs.DeleteAll();
-        Debug.Log("Data reset complete");
+        if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
+        {
+            File.Delete(Application.persistentDataPath + "/MySaveData.dat");
+            Debug.Log("Data reset complete!");
+        }
+        else
+        {
+            Debug.LogError("No save data to delete.");
+        }
     }
 
 
